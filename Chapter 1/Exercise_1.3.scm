55b04019abc600c3520c550982e8e7e20f3ef923
Ande <44785864+dissonancia@users.noreply.github.com>
2023-04-19 23:41:47 -0300

    Create Exercise_1.3.scm

---
+++ Chapter 1/Exercise_1.3.scm
@@ -0,0 +1,12 @@
+#!/usr/local/bin/guile -s!#
+
+(define (square x)
+  (* x x))
+
+(define (sum-squares x y)
+  (+ (square x) (square y)))
+
+(define (fn x y z)
+  (cond ((and (<= x y) (<= x z)) (sum-squares y z))
+	((and (<= y x) (<= y z)) (sum-squares x z))
+	(else                    (sum-squares x y))))
