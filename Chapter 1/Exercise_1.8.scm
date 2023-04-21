02b11e62aef97bf271918c51c5c6072ba6f56da7
Ande <44785864+dissonancia@users.noreply.github.com>
2023-04-21 13:41:29 -0300

    Create Exercise_1.8.scm

---
+++ Chapter 1/Exercise_1.8.scm
@@ -0,0 +1,17 @@
+#!/usr/local/bin/guile -s!#
+
+(define (square x) (* x x))
+
+(define (cube-root-iter guess old-guess x)
+  (if (good-enough? guess old-guess)
+      guess
+      (cube-root-iter (improve guess x) guess x)))
+
+(define (improve guess x)
+  (/ (+(/ x (square guess)) (* 2 guess)) 3))
+
+(define (good-enough? guess old-guess)
+  (< (abs (- guess old-guess)) 0.00000000001))
+
+(define (cube-root x)
+  (cube-root-iter 1.0 0.0 x))
