17f1451cff117bc10a9f771351e66e573bfa2197
Ande <44785864+dissonancia@users.noreply.github.com>
2023-04-22 11:04:41 -0300

    Update Exercise_1.9.scm

--- Chapter 1/1.2/Exercise_1.9.scm
+++ Chapter 1/1.2/Exercise_1.9.scm
@@ -8,11 +8,11 @@
       b
       (inc (+ (dec a) b))))
 
-;; (+ 4 5)
-;; (inc (+ 3 5))
-;; (inc (inc (+ 2 5)))
-;; (inc (inc (inc (+ 1 5))))
-;; (inc (inc (inc (inc (+ 0 5)))))
+;; (+rec 4 5)
+;; (inc (+rec 3 5))
+;; (inc (inc (+rec 2 5)))
+;; (inc (inc (inc (+rec 1 5))))
+;; (inc (inc (inc (inc (+rec 0 5)))))
 ;; (inc (inc (inc (inc 5))))
 ;; (inc (inc (inc 6)))
 ;; (inc (inc 7))
@@ -24,9 +24,9 @@
       b
       (+ (dec a) (inc b))))
 
-;; (+ 4 5)
-;; (+ 3 6)
-;; (+ 2 7)
-;; (+ 1 8)
-;; (+ 0 9)
+;; (+iter 4 5)
+;; (+iter 3 6)
+;; (+iter 2 7)
+;; (+iter 1 8)
+;; (+iter 0 9)
 ;; 9
