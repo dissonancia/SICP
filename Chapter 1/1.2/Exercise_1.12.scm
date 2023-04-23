5df9f69bf660d2f0c3b39640f4962d70f39bda73
Ande <44785864+dissonancia@users.noreply.github.com>
2023-04-22 21:44:40 -0300

    Update Exercise_1.12.scm

--- Chapter 1/1.2/Exercise_1.12.scm
+++ Chapter 1/1.2/Exercise_1.12.scm
@@ -5,3 +5,12 @@
 	((or (= col 1) (= col row)) 1)
 	(else (+ (pascal (- row 1) (- col 1))
 		 (pascal (- row 1) col)))))
+
+;; it's easy to reasoning if the Pascal’s triangle
+;; is in this form:
+;; 1
+;; 1 1
+;; 1 2 1
+;; 1 3 3 1
+;; 1 4 6 4 1
+;;   . . .
